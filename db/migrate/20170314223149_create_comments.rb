70e43571e4bc3332a482ad888bbf312627525adc
andresback <aback@uc.cl>
2017-03-14 19:53:58 -0300

    fix in migration

--- db/migrate/20170314223149_create_comments.rb
+++ db/migrate/20170314223149_create_comments.rb
@@ -3,7 +3,6 @@ class CreateComments < ActiveRecord::Migration[5.0]
     create_table :comments do |t|
       t.string :name
       t.text :comment
-      t.news :belongs_to
 
       t.timestamps
     end
